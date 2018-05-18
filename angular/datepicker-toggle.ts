a31ee84bd96410060746b58b851701c20c622e35
Matthew Toll <matthew.toll83@gmail.com>
2018-05-18 11:15:38 -0400

    Add files via upload
    bringing in source code for angular material version 5.2 datepicker files

---
+++ angular/datepicker-toggle.ts
@@ -0,0 +1,109 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {coerceBooleanProperty} from '@angular/cdk/coercion';
+import {
+  AfterContentInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Input,
+  OnChanges,
+  OnDestroy,
+  SimpleChanges,
+  ViewEncapsulation,
+  Directive,
+  ContentChild,
+} from '@angular/core';
+import {merge} from 'rxjs/observable/merge';
+import {of as observableOf} from 'rxjs/observable/of';
+import {Subscription} from 'rxjs/Subscription';
+import {MatDatepicker} from './datepicker';
+import {MatDatepickerIntl} from './datepicker-intl';
+
+
+/** Can be used to override the icon of a `matDatepickerToggle`. */
+@Directive({
+  selector: '[matDatepickerToggleIcon]'
+})
+export class MatDatepickerToggleIcon {}
+
+
+@Component({
+  moduleId: module.id,
+  selector: 'mat-datepicker-toggle',
+  templateUrl: 'datepicker-toggle.html',
+  styleUrls: ['datepicker-toggle.css'],
+  host: {
+    'class': 'mat-datepicker-toggle',
+    '[class.mat-datepicker-toggle-active]': 'datepicker && datepicker.opened',
+  },
+  exportAs: 'matDatepickerToggle',
+  encapsulation: ViewEncapsulation.None,
+  preserveWhitespaces: false,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MatDatepickerToggle<D> implements AfterContentInit, OnChanges, OnDestroy {
+  private _stateChanges = Subscription.EMPTY;
+
+  /** Datepicker instance that the button will toggle. */
+  @Input('for') datepicker: MatDatepicker<D>;
+
+  /** Whether the toggle button is disabled. */
+  @Input()
+  get disabled(): boolean {
+    return this._disabled === undefined ? this.datepicker.disabled : !!this._disabled;
+  }
+  set disabled(value: boolean) {
+    this._disabled = coerceBooleanProperty(value);
+  }
+  private _disabled: boolean;
+
+  /** Custom icon set by the consumer. */
+  @ContentChild(MatDatepickerToggleIcon) _customIcon: MatDatepickerToggleIcon;
+
+  constructor(public _intl: MatDatepickerIntl, private _changeDetectorRef: ChangeDetectorRef) {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.datepicker) {
+      this._watchStateChanges();
+    }
+  }
+
+  ngOnDestroy() {
+    this._stateChanges.unsubscribe();
+  }
+
+  ngAfterContentInit() {
+    this._watchStateChanges();
+  }
+
+  _open(event: Event): void {
+    if (this.datepicker && !this.disabled) {
+      this.datepicker.open();
+      event.stopPropagation();
+    }
+  }
+
+  private _watchStateChanges() {
+    const datepickerDisabled = this.datepicker ? this.datepicker._disabledChange : observableOf();
+    const inputDisabled = this.datepicker && this.datepicker._datepickerInput ?
+        this.datepicker._datepickerInput._disabledChange : observableOf();
+    const datepickerToggled = this.datepicker ?
+        merge(this.datepicker.openedStream, this.datepicker.closedStream) :
+        observableOf();
+
+    this._stateChanges.unsubscribe();
+    this._stateChanges = merge(
+      this._intl.changes,
+      datepickerDisabled,
+      inputDisabled,
+      datepickerToggled
+    ).subscribe(() => this._changeDetectorRef.markForCheck());
+  }
+}
---
+++ angular/month-view.spec.ts
@@ -0,0 +1,119 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {Component} from '@angular/core';
+import {By} from '@angular/platform-browser';
+import {MatMonthView} from './month-view';
+import {MatCalendarBody} from './calendar-body';
+import {MatNativeDateModule} from '@angular/material/core';
+import {JAN, MAR} from '@angular/material/core';
+
+describe('MatMonthView', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        MatNativeDateModule,
+      ],
+      declarations: [
+        MatCalendarBody,
+        MatMonthView,
+
+        // Test components.
+        StandardMonthView,
+        MonthViewWithDateFilter,
+      ],
+    });
+
+    TestBed.compileComponents();
+  }));
+
+  describe('standard month view', () => {
+    let fixture: ComponentFixture<StandardMonthView>;
+    let testComponent: StandardMonthView;
+    let monthViewNativeElement: Element;
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(StandardMonthView);
+      fixture.detectChanges();
+
+      let monthViewDebugElement = fixture.debugElement.query(By.directive(MatMonthView));
+      monthViewNativeElement = monthViewDebugElement.nativeElement;
+      testComponent = fixture.componentInstance;
+    });
+
+    it('has correct month label', () => {
+      let labelEl = monthViewNativeElement.querySelector('.mat-calendar-body-label')!;
+      expect(labelEl.innerHTML.trim()).toBe('JAN');
+    });
+
+    it('has 31 days', () => {
+      let cellEls = monthViewNativeElement.querySelectorAll('.mat-calendar-body-cell')!;
+      expect(cellEls.length).toBe(31);
+    });
+
+    it('shows selected date if in same month', () => {
+      let selectedEl = monthViewNativeElement.querySelector('.mat-calendar-body-selected')!;
+      expect(selectedEl.innerHTML.trim()).toBe('10');
+    });
+
+    it('does not show selected date if in different month', () => {
+      testComponent.selected = new Date(2017, MAR, 10);
+      fixture.detectChanges();
+
+      let selectedEl = monthViewNativeElement.querySelector('.mat-calendar-body-selected');
+      expect(selectedEl).toBeNull();
+    });
+
+    it('fires selected change event on cell clicked', () => {
+      let cellEls = monthViewNativeElement.querySelectorAll('.mat-calendar-body-cell');
+      (cellEls[cellEls.length - 1] as HTMLElement).click();
+      fixture.detectChanges();
+
+      let selectedEl = monthViewNativeElement.querySelector('.mat-calendar-body-selected')!;
+      expect(selectedEl.innerHTML.trim()).toBe('31');
+    });
+
+    it('should mark active date', () => {
+      let cellEls = monthViewNativeElement.querySelectorAll('.mat-calendar-body-cell');
+      expect((cellEls[4] as HTMLElement).innerText.trim()).toBe('5');
+      expect(cellEls[4].classList).toContain('mat-calendar-body-active');
+    });
+  });
+
+  describe('month view with date filter', () => {
+    let fixture: ComponentFixture<MonthViewWithDateFilter>;
+    let monthViewNativeElement: Element;
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(MonthViewWithDateFilter);
+      fixture.detectChanges();
+
+      let monthViewDebugElement = fixture.debugElement.query(By.directive(MatMonthView));
+      monthViewNativeElement = monthViewDebugElement.nativeElement;
+    });
+
+    it('should disable filtered dates', () => {
+      let cells = monthViewNativeElement.querySelectorAll('.mat-calendar-body-cell');
+      expect(cells[0].classList).toContain('mat-calendar-body-disabled');
+      expect(cells[1].classList).not.toContain('mat-calendar-body-disabled');
+    });
+  });
+});
+
+
+@Component({
+  template: `<mat-month-view [activeDate]="date" [(selected)]="selected"></mat-month-view>`,
+})
+class StandardMonthView {
+  date = new Date(2017, JAN, 5);
+  selected = new Date(2017, JAN, 10);
+}
+
+
+@Component({
+  template: `<mat-month-view [activeDate]="activeDate" [dateFilter]="dateFilter"></mat-month-view>`
+})
+class MonthViewWithDateFilter {
+  activeDate = new Date(2017, JAN, 1);
+  dateFilter(date: Date) {
+    return date.getDate() % 2 == 0;
+  }
+}
